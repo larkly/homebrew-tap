819331c52067329e7c5812d1c45d678e76e74808
Bosse Klykken <bosse@klykken.com>
2026-05-20 08:54:13 +0200

    fix: restore lazystack formula to v0.9.1

--- Formula/lazystack.rb
+++ Formula/lazystack.rb
@@ -1,26 +1,26 @@
 class Lazystack < Formula
   desc "Fast, keyboard-driven TUI for managing OpenStack resources"
   homepage "https://github.com/larkly/lazystack"
-  version ""
+  version "0.9.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/larkly/lazystack/releases/download/v#{version}/lazystack-darwin-arm64"
-      sha256 ""
+      sha256 "3ee1fd9b6f8e0e25912603226f4680fba9b3b74a522017e1c789ee9c8b5abf70"
     elsif Hardware::CPU.intel?
       url "https://github.com/larkly/lazystack/releases/download/v#{version}/lazystack-darwin-amd64"
-      sha256 ""
+      sha256 "ceccf659ae9c4b52731cc23a636a4a087abe4f0a4ccfb7b26d68a2c05f44b803"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/larkly/lazystack/releases/download/v#{version}/lazystack-linux-arm64"
-      sha256 ""
+      sha256 "33aca622ed81608531b9f0dbdc8b762dacfb312623b5ec8ecfab9371c503d4b5"
     elsif Hardware::CPU.intel?
       url "https://github.com/larkly/lazystack/releases/download/v#{version}/lazystack-linux-amd64"
-      sha256 ""
+      sha256 "4e0553565060c7ca02253a95cd3bf57c1992a1df1fef493338558b1631948dd0"
     end
   end
 
